a4a2dd74d48142ffcbd49ad659364b0d7cbbeb92
mashaomin <m623606643@163.com>
2017-09-27 10:36:00 +0800

    转移项目库

---
+++ Assets/Scripts/summer_base_common/Skill/AtomAction/Camera/PlayCameraOffset.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Summer
+{
+    public class PlayCameraOffset : ASkillAction
+    {
+        public override void OnEnter()
+        {
+
+        }
+
+        public override void OnExit()
+        {
+
+        }
+
+        public override void OnUpdate(float dt)
+        {
+            
+        }
+    }
+
+}
