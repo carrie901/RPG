de4998749e26343c28be794f6301f9cc1a4b00d6
mashaomin <m623606643@163.com>
2018-03-21 14:57:55 +0800

    update

---
+++ Assets/Scripts/Summer/summer_base_data/Const/EffConst.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//=============================================================================
+// Author : mashao
+// CreateTime : 2018-2-27 15:8:17
+// FileName : EffConst.cs
+//=============================================================================
+
+namespace Summer
+{
+    /// <summary>
+    /// 特效的名词引用
+    /// </summary>
+    public class EffConst
+    {
+        public static string eff_upgrade_success_01 = "prefab/ui_eff/eff_quality_success_01";
+    }
+}
---
+++ Assets/Scripts/Summer/summer_base_data/Const/LocConst.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//=============================================================================
+// Author : mashao
+// CreateTime : 2018-2-27 11:17:39
+// FileName : LocConst.cs
+//=============================================================================
+
+namespace Summer
+{
+    public class LocConst
+    {
+        public static string loc_need_level = "需要Lv.{0}";
+
+        public static string quality_upgrade_no_engouh_material = "没有足够的材料";
+
+        public static string quality_upgrade_no_engouh_coin = "没有足够的金币";
+
+        public static string quality_upgrade_no_engouh_hero_level = "前置等级不足";
+
+        public static string quality_upgrade_max_level = "已经达到最大等级";
+    }
+}
