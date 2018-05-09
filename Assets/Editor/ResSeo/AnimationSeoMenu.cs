5ffc4744c9c627ffc5f1c75222b2c2ded68ad1ea
mashaomin <m623606643@163.com>
2018-05-09 18:43:15 +0800

    fbx动画分离和AnimationClip裁剪精度和剔除Scale曲线

---
+++ Assets/Editor/ResSeo/AnimationSeoMenu.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace SummerEditor
+{
+    public class AnimationSeoMenu
+    {
+        public const string FLOAT_FORMAT = "f3";                            // 裁剪精度
+        public static string anim_directory = "Assets/Raw/Animation/";
+        public const string SUFFIX_ANIM = ".anim";
+        [MenuItem("Tools/优化/动画内存优化")]
+        public static void AllSeoAnimation()
+        {
+
+            List<string> anim_paths = new List<string>();
+            EPathHelper.ScanDirectoryFile(anim_directory, true, anim_paths, SUFFIX_ANIM);
+
+            int length = anim_paths.Count;
+            for (int i = 0; i < length; i++)
+            {
+                string seo_path = EPathHelper.AbsoluteToRelativePathWithAssets(anim_paths[i]);
+                AnimationClip clip = AssetDatabase.LoadAssetAtPath<AnimationClip>(seo_path);
+                if (clip == null) continue;
+                _seo_animation(clip);
+                EditorUtility.DisplayProgressBar(seo_path, string.Format("动画内存优化({0}/{1})", i + 1, length), (float)(i + 1) / length);
+            }
+            ShowDialog();
+        }
+
+        [MenuItem("Assets/优化/动画内存优化")]
+        public static void SeoAnimation()
+        {
+            AnimationClip clip = Selection.activeObject as AnimationClip;
+            if (clip == null) return;
+            _seo_animation(clip);
+            ShowDialog();
+        }
+
+        public static void ShowDialog()
+        {
+            AssetDatabase.Refresh();
+            AssetDatabase.SaveAssets();
+            EditorUtility.ClearProgressBar();
+            EditorUtility.DisplayDialog("", "动画内存优化完毕", "OK");
+        }
+
+        // 剔除Scale曲线和裁剪精度
+        public static bool _seo_animation(AnimationClip anim_clip)
+        {
+            if (anim_clip == null) return false;
+
+
+            bool result = false;
+            try
+            {
+                EditorCurveBinding[] anim_curves = AnimationUtility.GetCurveBindings(anim_clip);
+                //去除scale曲线
+                foreach (EditorCurveBinding the_curve_binding in anim_curves)
+                {
+                    string name = the_curve_binding.propertyName.ToLower();
+                    if (name.Contains("scale"))
+                    {
+                        AnimationUtility.SetEditorCurve(anim_clip, the_curve_binding, null);
+                    }
+                }
+
+                //浮点数精度压缩到f3
+                foreach (EditorCurveBinding curve_binding in anim_curves)
+                {
+                    AnimationCurve curve_data = AnimationUtility.GetEditorCurve(anim_clip, curve_binding);
+                    if (curve_data == null || curve_data.keys == null) continue;
+                    Keyframe[] key_frames = curve_data.keys;
+                    for (int i = 0; i < key_frames.Length; i++)
+                    {
+                        Keyframe key_frame = key_frames[i];
+                        key_frame.value = float.Parse(key_frame.value.ToString(FLOAT_FORMAT));
+                        key_frame.inTangent = float.Parse(key_frame.inTangent.ToString(FLOAT_FORMAT));
+                        key_frame.outTangent = float.Parse(key_frame.outTangent.ToString(FLOAT_FORMAT));
+                        key_frames[i] = key_frame;
+                    }
+                    curve_data.keys = key_frames;
+
+                    anim_clip.SetCurve(curve_binding.path, curve_binding.type, curve_binding.propertyName, curve_data);
+                }
+                result = true;
+
+            }
+            catch (System.Exception e)
+            {
+                Debug.LogError(string.Format("剔除Scale数据并且浮点数精度压缩到f3失败: {0} error: {1}", anim_clip.name, e));
+            }
+            return result;
+        }
+
+
+    }
+}
+
---
+++ Assets/Editor/summer_editor/Common/EMemorySizeHelper.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Profiling;
+using Object = UnityEngine.Object;
+
+namespace SummerEditor
+{
+    public class EMemorySizeHelper
+    {
+
+        public static long GetMemorySize(Object o)
+        {
+            return Profiler.GetRuntimeMemorySizeLong(o);
+        }
+
+        public static float GetFileSize(string path)
+        {
+            if (!File.Exists(path)) return 0;
+            FileInfo info = new FileInfo(path);
+            return info.Length;
+        }
+
+        public static Assembly asm;
+        public static MethodInfo get_animation_clip_stats;
+        public static Type aniclipstats;
+        public static FieldInfo size_info;
+
+        public static long GetBlobSize(AnimationClip anim_clip)
+        {
+            if (asm == null)
+                asm = Assembly.GetAssembly(typeof(Editor));
+            if (get_animation_clip_stats == null)
+                get_animation_clip_stats = typeof(AnimationUtility).GetMethod("GetAnimationClipStats", BindingFlags.Static | BindingFlags.NonPublic);
+            if (aniclipstats == null)
+                aniclipstats = asm.GetType("UnityEditor.AnimationClipStats");
+            if (size_info == null)
+                size_info = aniclipstats.GetField("size", BindingFlags.Public | BindingFlags.Instance);
+
+            if (get_animation_clip_stats == null || size_info == null) return 0;
+            var stats = get_animation_clip_stats.Invoke(null, new object[] { anim_clip });
+            int blob_size = (int)(size_info.GetValue(stats));
+            return blob_size;
+        }
+
+        public static string GetKb(int bytes)
+        {
+            string size = (bytes / 1024).ToString("f2") + " Kb";
+            if ((bytes / 1024) > 1024)
+                size = ((float)bytes / (1024 * 1024)).ToString("f2") + " Mb";
+            return size;
+        }
+
+
+    }
+
+}
